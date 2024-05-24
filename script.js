4a8fc13d519468e54d9376d21f24a9ce664876b8
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-05-23 21:34:33 -0400

    work on the map for the website

--- script.js
+++ script.js
@@ -65,3 +65,68 @@ form.addEventListener("submit", (e) => {
     }
 });
 
+// AP working on how to set up a map in the website
+
+ avigator.geolocation.getCurrentPosition(function(position) {
+    var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+ }});
+
+
+    map.setCenter(pos);
+    //Put marker of the Geolocated user location
+    var userMarker = new google.maps.Marker({
+        map: map,
+        position: pos
+    });
+
+    google.maps.event.addListener(userMarker, 'click', function() {
+        infoWindow.setContent('Your location');
+        infoWindow.open(map, this);
+    });
+
+    var requestMoviesTheater = {
+        location: pos,
+        radius: '10000',
+        type: ['moviesTheater']
+    };
+
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(requestmoviesTheater, callback);
+
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            for (var i = 0; i < results.length; i++) {
+                var place = results[i];
+                createMarker(results[i]);
+            }
+        }
+    }
+
+    var type = place.types;
+    var iconStyle;
+    //loop to all the type of the place
+    for (var i = 0; i < type.length; i++) {
+        //put array of Place types in placeType variable
+        var placeType = type[i];        
+        //Check the placeType and set the icon according to the placeType value
+        switch (placeType) {
+            case "moviesTheater":
+                iconStyle = "/v2/showtimes/views/current-location/2-24-2017/38.91/-94.64"
+                // iconStyle = "http://maps.google.com/mapfiles/kml/shapes/pharmacy_rx.png"
+                break;
+        }}
+
+        //put marker of the places in the map
+    var marker = new google.maps.Marker({
+        map: map,
+        icon: iconStyle,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function() {
+        infoWindow.setContent(place.name);
+        infoWindow.open(map, this);
+    });
+// this where i stop working for today 05/23/24
